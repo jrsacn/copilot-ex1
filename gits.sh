5d6256f021340c8fee49be8773f3e1c3a30cc81c
jrsacn <jesus.r.salazar@accenture.com>
2026-07-26 02:09:39 +0000

    Bash: Added `gits.sh` with aliases

---
+++ gits.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+alias ls='ls -AF'
+alias ll='ls -lAF'
+alias md='mkdir'
+alias rd='rmdir'
+alias vi='vi -c ":set nu"'
+
+alias g='git'
+
+alias gadd='git add'
+
+alias gbra='git branch'
+alias gbrac='git branch --column'
+
+alias gcom='git commit'
+alias gcomm='git commit -m'
+
+alias gdif='git diff -w --abbrev'
+alias gdiff='git diff -w --abbrev --stat'
+
+alias glog='git log --abbrev-commit --pretty=oneline'
+
+alias gres='git restore'
+
+alias gsta='git status -bs --no-renames'
+
